9678e37dde07a8657380252b0133b6619e09c393
Faraz Bukhari <faraz.bukhari@conovoinc.com>
2024-09-03 19:08:13 +0500

    ui fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { faUnity } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center p-5 bg-slate-800'>
+		<main className='flex min-h-screen flex-col items-center p-5'>
 			<div className='flex items-center gap-4'>
 				<FontAwesomeIcon icon={faUnity} className='text-3xl text-white' />
 				<h1 className='text-lg text-white font-thin'>Unity WebGL</h1>
