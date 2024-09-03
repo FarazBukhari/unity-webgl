201b36e2f077983ed91449e1eb4dcc43e9cfe085
Faraz Bukhari <faraz.bukhari@conovoinc.com>
2024-09-03 18:27:38 +0500

    fixed device pixel ratio

--- components/WebGL.tsx
+++ components/WebGL.tsx
@@ -9,7 +9,6 @@ const streamingAssestLocation: string = "unity/StreamingAssets";
 const fileName: string = "TestBuild";
 
 const WebGL = () => {
-	const [devicePixelRatio, setDevicePixelRatio] = useState<number | null>(null);
 	const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
 	const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
@@ -38,27 +37,6 @@ const WebGL = () => {
 		setDimensions({ width, height });
 	};
 
-	useEffect(
-		function () {
-			// A function which will update the device pixel ratio of the Unity
-			// Application to match the device pixel ratio of the browser.
-			const updateDevicePixelRatio = function () {
-				setDevicePixelRatio(window.devicePixelRatio);
-			};
-			// A media matcher which watches for changes in the device pixel ratio.
-			const mediaMatcher = window.matchMedia(`screen and (resolution: ${devicePixelRatio}dppx)`);
-			// Adding an event listener to the media matcher which will update the
-			// device pixel ratio of the Unity Application when the device pixel
-			// ratio changes.
-			mediaMatcher.addEventListener("change", updateDevicePixelRatio);
-			return function () {
-				// Removing the event listener when the component unmounts.
-				mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
-			};
-		},
-		[devicePixelRatio]
-	);
-
 	useEffect(() => {
 		calculateDimensions();
 		window.addEventListener("resize", calculateDimensions);
@@ -80,7 +58,7 @@ const WebGL = () => {
 			<Unity
 				unityProvider={unityProvider}
 				className='unity-canvas'
-				devicePixelRatio={devicePixelRatio!}
+				devicePixelRatio={3}
 				style={{
 					height: `${dimensions.height}px`,
 					width: `${dimensions.width}px`,
